8b56f8d0660a05c4f44bc506a91519f23817d565
Lucas Henrique <43302698+lucashenriquecss@users.noreply.github.com>
2024-12-28 10:25:11 -0300

    feat(controller): createauctioncontroller

---
+++ src/main/java/com/example/mercatusAPI/controller/AuctionController.java
@@ -0,0 +1,40 @@
+package com.example.mercatusAPI.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.mercatusAPI.entitty.auction.Auction;
+import com.example.mercatusAPI.service.AuctionService;
+
+@RestController
+@RequestMapping("/api/auctions")
+public class AuctionController {
+
+    private final AuctionService auctionService;
+
+    @Autowired
+    public AuctionController(AuctionService auctionService) {
+        this.auctionService = auctionService;
+    }
+
+    @PostMapping("/{auctionId}/bid")
+    public ResponseEntity<String> placeBid(@PathVariable String auctionId, @RequestBody double bidAmount) {
+        Auction auction = auctionService.findAuctionById(auctionId);
+        auctionService.placeBid(null, auction, bidAmount); 
+
+        return ResponseEntity.ok("Bid placed successfully");
+    }
+
+    @GetMapping("/open")
+    public List<Auction> getOpenAuctions() {
+        return auctionService.getOpenAuctions();
+    }
+}
