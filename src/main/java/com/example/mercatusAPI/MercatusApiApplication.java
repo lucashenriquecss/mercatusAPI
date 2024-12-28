b615466f11c4a980010a2c3ff08b9b97115e29c7
Lucas Henrique <43302698+lucashenriquecss@users.noreply.github.com>
2024-12-28 08:09:33 -0300

    Initial commit

---
+++ src/main/java/com/example/mercatusAPI/MercatusApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.mercatusAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercatusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercatusApiApplication.class, args);
+	}
+
+}
